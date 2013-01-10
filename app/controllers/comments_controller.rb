7baa82749f01a68eaf1614ec710cfb94b053803f
Systest RJR <systestrjr@gmail.com>
2013-01-10 15:20:12 -0500

    modified with shared/_error_messages

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -8,16 +8,33 @@ class CommentsController < ApplicationController
   end
 
   def create
-    post = Post.find(params[:post_id])
-    post.comments.create(params[:comment])
+    @post = Post.find(params[:post_id])
+    @comment = @post.comments.create(params[:comment])
 
-    if post.save
-      redirect_to post_path(post)
+
+    if @comment.save
+      #redirect_to posts_path , notice: 'New comment was successfully saved'
+      redirect_to post_path(@post) , notice: 'New comment was successfully saved'
     else
-      flash[:alert] = "Your post could not be saved: " +
-          post.errors.full_messages.to_sentence
-      #render 'new'
-      redirect_to post_path(post)
+      @post = Post.find(params[:post_id])
+      render 'posts/show'
     end
   end
+
+
+
+  #-----------------------
+  #
+  #def create
+  #  @comment = Comment.new(params[:comment])
+  #  @comment.post = Post.where(slug: params[:post_id]).first
+  #  if @comment.save
+  #    redirect_to @comment.post, notice: 'New comment was succesfully saved'
+  #  else
+  #    @post = @comment.post
+  #    render 'posts/show'
+  #  end
+  #end
+
+
 end
